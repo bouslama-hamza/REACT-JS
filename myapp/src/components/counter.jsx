6d94dbab74abda290ac97cc074480cc21ebec203
bouslama-hamza <ham.bousa98@gmail.com>
2022-01-17 23:07:29 +0100

    understand conecpts

--- myapp/src/components/counter.jsx
+++ myapp/src/components/counter.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count : 3
+    };
+
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+            <React.Fragment>
+                <h1 className={this.getcountnumber()}>{this.makecount()}</h1> 
+                <button className="btn btn-secondary btn-sm">Incriment</button>
+            </React.Fragment>
+        );
+    }
+
+    getcountnumber() {
+        let countes = "badge m-2 badge-";
+        countes += this.state.count === 0 ? "warning" : "primary";
+        return countes;
+    }
+
+    makecount(){
+        return this.state.count === 0 ? "Zero" : this.state.count;
     }
 }
  
