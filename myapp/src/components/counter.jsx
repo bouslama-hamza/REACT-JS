19c13d65da62445780258f3e53f1debb31305d55
bouslama-hamza <ham.bousa98@gmail.com>
2022-01-25 02:30:46 +0100

    Finish First Project

--- myapp/src/components/counter.jsx
+++ myapp/src/components/counter.jsx
@@ -2,26 +2,39 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count : 3
+        value : this.props.counter.value
     };
+
+    handleIncriment = () => {
+        this.setState({value : this.state.value + 1})
+    }
 
     render() { 
         return (
             <React.Fragment>
                 <h1 className={this.getcountnumber()}>{this.makecount()}</h1> 
-                <button className="btn btn-secondary btn-sm">Incriment</button>
+                <button 
+                    onClick={this.handleIncriment} 
+                    className="btn btn-secondary btn-sm">
+                    Incriment
+                </button>
+                <button 
+                    onClick={() => this.props.onDelete(this.props.counter.id)} 
+                    className="btn btn-danger btn-sm m-2">
+                    Danger
+                </button>
             </React.Fragment>
         );
     }
 
     getcountnumber() {
         let countes = "badge m-2 badge-";
-        countes += this.state.count === 0 ? "warning" : "primary";
+        countes += this.state.value === 0 ? "warning" : "primary";
         return countes;
     }
 
     makecount(){
-        return this.state.count === 0 ? "Zero" : this.state.count;
+        return this.state.value === 0 ? "Zero" : this.state.value;
     }
 }
  
